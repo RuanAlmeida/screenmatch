c49d1878841b543a57d3893053b4cf93c6cbf8f3
Ruan Almeida <ruan.aalmeidasantos@gmail.com>
2026-03-02 18:28:01 -0300

    FileWriter

--- src/alura/screenmatch/Principal/PrincipalComBusca.java
+++ src/alura/screenmatch/Principal/PrincipalComBusca.java
@@ -13,53 +13,70 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.IllegalFormatException;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        try{
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite qual filme deseja assistir: ");
-        var busca = leitura.nextLine();
-
-        var endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=66106cea";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-      // System.out.println(json);
-
+        String busca = " ";
+        List<Titulo> titulos = new ArrayList<>();
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
                 .create();
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
+
+        while (!busca.equalsIgnoreCase("Sair")) {
+            System.out.println("Digite qual filme deseja assistir: ");
+            busca = leitura.nextLine();
+
+            if (busca.equalsIgnoreCase("Sair")){
+                break;
+            }
+
+            var endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=66106cea";
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                // System.out.println(json);
 
 
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println(meuTitulo);
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(meuTituloOmdb);
 
-            FileWriter escrita = new FileWriter("filmes.txt");
-            escrita.write(meuTitulo.toString());
-            escrita.close();
 
-        } catch (NumberFormatException e){
-            System.out.println("Aconteceu um erro: ");
-            System.out.println(e.getMessage());
-        } catch (IllegalFormatException e){
-            System.out.println("Erro na formatação na busca: ");
-            System.out.println(e.getMessage());
-        } catch (ErrorDeConversorDeAnoException e){
-            System.out.println(e.getMensagem());
-        } finally {
-            System.out.println("Programa finalizado!");
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println(meuTitulo);
+
+                titulos.add(meuTitulo);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalFormatException e) {
+                System.out.println("Erro na formatação na busca: ");
+                System.out.println(e.getMessage());
+            } catch (ErrorDeConversorDeAnoException e) {
+                System.out.println(e.getMensagem());
+            }
         }
+
+
+        System.out.println(titulos);
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+
 
 
 
--- src/alura/screenmatch/modelos/Titulo.java
+++ src/alura/screenmatch/modelos/Titulo.java
@@ -91,7 +91,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome= " + nome +
-                ", anoDeLancamento= " + anoDeLancamento + ", " + "Duração em minutos: " + duracaoEmMinutos;
+        return "( nome = " + nome +
+                ", anoDeLancamento = " + anoDeLancamento + ", " + "Duração em minutos: " + duracaoEmMinutos
+                + " )";
     }
 }
